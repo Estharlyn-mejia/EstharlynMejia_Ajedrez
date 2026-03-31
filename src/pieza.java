661571667c029ca11ee28b9be17a47f7f5a902c5
Estharlyn Mejia Regalado <estharlyn_mejia@ucne.edu.do>
2026-03-31 15:37:18 -0400

    Agregue ficha y el tablero

---
+++ src/pieza.java
@@ -0,0 +1,17 @@
+public class pieza {
+    private String color;
+    private String tipo;
+
+    public pieza(String color, String tipo) {
+        this.color = color;
+        this.tipo = tipo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+}
